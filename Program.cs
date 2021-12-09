8a20e990ab7ffd49fef48769a2d7fed711c423cc
Davi Conti <davi.alvesskt@gmail.com>
2021-12-08 21:13:24 -0300

    Exercício Soma de Despesas

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resp;
+            double total_disp = 0;
+            do
+            {
+                Console.WriteLine("Descrição da despesa:");
+                string despesa = Console.ReadLine();
+                Console.WriteLine("Valor da despesa:");
+                double valor = double.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Deseja adicionar outra despesa ?");
+                resp = Console.ReadLine().ToUpper();
+
+                total_disp += valor;
+            }
+
+            while (resp == "SIM");
+
+            Console.WriteLine("Valor total de despesas: " + total_disp);
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
